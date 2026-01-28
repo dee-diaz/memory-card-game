920c1967a43676deb359b1a604c385aac3d3c3bb
dee-diaz <diazthediablo@yahoo.com>
2026-01-28 16:24:28 -0300

    feat(ModeSelection): create mode selection component with difficulty levels

--- src/components/ModeSelection.jsx
+++ src/components/ModeSelection.jsx
@@ -1,1 +1,25 @@
-export default function ModeSelection() {}
+import ModeButton from './ModeButton';
+
+export const LEVEL = {
+  EASY: 'Easy',
+  MEDIUM: 'Medium',
+  HARD: 'Hard',
+};
+
+export default function ModeSelection() {
+  return (
+    <div className="grid gap-16 justify-center">
+      <div className="grid gap-8 justify-center">
+        <img className="w-xs lg:w-xl" src="/logo.webp" alt="Pokemon logo" />
+        <h1 className="text-center text-2xl text-(--clr-text-blue) uppercase">
+          Memory card game
+        </h1>
+      </div>
+      <div className="grid justify-center gap-6 lg:flex">
+        <ModeButton level={LEVEL.EASY} />
+        <ModeButton level={LEVEL.MEDIUM} />
+        <ModeButton level={LEVEL.HARD} />
+      </div>
+    </div>
+  );
+}
