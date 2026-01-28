192c5067d899a1c405aa8d9371dc601793c8eb1a
dee-diaz <diazthediablo@yahoo.com>
2026-01-28 16:24:58 -0300

    feat(App): integrate main components and add background layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import { useState } from 'react';
-import './App.css';
+import SoundControl from './components/SoundControl';
+import ModeSelection from './components/ModeSelection';
+import Footer from './components/Footer';
 
-function App() {}
+function App() {
+  return (
+    <div className="bg-[url(/bg-image.webp)] bg-no-repeat bg-center bg-cover min-h-screen flex flex-col justify-between px-4 py-4">
+      <SoundControl />
+      <ModeSelection />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
