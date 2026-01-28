277dbadec97bb6ced79ad8a9f48937bf5965a797
dee-diaz <diazthediablo@yahoo.com>
2026-01-28 17:22:17 -0300

    feat: rename LEVEL to MODE and add mode selection handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react';
+import { MODE } from './components/ModeSelection';
 import SoundControl from './components/SoundControl';
 import ModeSelection from './components/ModeSelection';
 import Footer from './components/Footer';
 
 function App() {
+  const [mode, setMode] = useState(null);
+
+  function handleModeSelection(value) {
+    setMode(value);
+  }
+
   return (
     <div className="bg-[url(/bg-image.webp)] bg-no-repeat bg-center bg-cover min-h-screen flex flex-col justify-between px-4 py-4 before:absolute before:inset-0 before:w-full before:h-full before:bg-(--clr-overlay)">
       <SoundControl />
-      <ModeSelection />
+      <ModeSelection onClick={handleModeSelection} />
       <Footer />
     </div>
   );
