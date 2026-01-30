1c701535e50eda81c1888b2405bed3af66cce285
dee-diaz <diazthediablo@yahoo.com>
2026-01-30 12:26:55 -0300

    refactor: extract layout wrapper with background into separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Layout from './components/Layout';
 import SoundButton from './components/SoundControl';
 import ModeSelection from './components/ModeSelection';
 import Footer from './components/Footer';
@@ -23,10 +24,13 @@ function App() {
   }
 
   return (
-    <div className="bg-[url(/bg-image.webp)] bg-no-repeat bg-center bg-cover min-h-screen flex flex-col justify-between px-4 py-4 before:absolute before:inset-0 before:w-full before:h-full before:bg-(--clr-overlay)">
+    <Layout>
       {mode ? (
         <>
-          <Header onSoundToggle={() => setIsSoundOn(!isSoundOn)} isSoundOn={isSoundOn} />
+          <Header
+            onSoundToggle={() => setIsSoundOn(!isSoundOn)}
+            isSoundOn={isSoundOn}
+          />
           <Game />
           <Footer />
           {isGameOver && (
@@ -39,12 +43,18 @@ function App() {
         </>
       ) : (
         <>
-          <SoundButton onClick={() => setIsSoundOn(!isSoundOn)} isSoundOn={isSoundOn} />
-          <ModeSelection onClick={(value) => setMode(value)} isSoundOn={isSoundOn} />
+          <SoundButton
+            onClick={() => setIsSoundOn(!isSoundOn)}
+            isSoundOn={isSoundOn}
+          />
+          <ModeSelection
+            onClick={(value) => setMode(value)}
+            isSoundOn={isSoundOn}
+          />
           <Footer />
         </>
       )}
-    </div>
+    </Layout>
   );
 }
 
