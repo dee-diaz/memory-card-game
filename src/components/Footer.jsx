177de43828ffddb2440503169aa6e6dc34c82e58
dee-diaz <diazthediablo@yahoo.com>
2026-02-02 17:18:21 -0300

    refactor(Footer): simplify to show only game instructions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,8 @@
 export default function Footer() {
   return (
     <footer className="relative w-full flex justify-center">
-      <p className="uppercase font-normal text-white text-base">
-        Created by {' '}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Diana Diaz (opens in a new tab)"
-          href="https://github.com/dee-diaz"
-          className="transition-colors hover:text-amber-300"
-        >
-          Diana Diaz
-        </a>
+      <p className="font-normal text-white text-lg">
+        Click each card only once to win
       </p>
     </footer>
   );
