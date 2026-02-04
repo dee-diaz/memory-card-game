da5927b49d83a1a7a77222e3aed34574f0fa567b
dee-diaz <diazthediablo@yahoo.com>
2026-02-04 18:48:55 -0300

    fix(Layout): fix the background layover issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 export default function Layout({ children }) {
   return (
-    <div className="bg-[url(/bg-image.webp)] bg-no-repeat bg-center bg-cover min-h-screen flex flex-col justify-between px-4 py-4 before:absolute before:inset-0 before:w-full before:h-full before:bg-(--clr-overlay)">{children}</div>
+    <div className="relative bg-[url(/bg-image.webp)] bg-no-repeat bg-center bg-cover min-h-screen flex flex-col justify-between px-4 py-4 before:absolute before:inset-0 before:w-full before:h-full before:bg-(--clr-overlay)">{children}</div>
   );
 }
