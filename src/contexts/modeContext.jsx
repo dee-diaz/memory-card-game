f75ddd3a394cb4641afdc8ae3d318d47789ca07e
dee-diaz <diazthediablo@yahoo.com>
2026-02-05 10:54:48 -0300

    fix: disable eslint rule for context exports

--- src/contexts/modeContext.jsx
+++ src/contexts/modeContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from 'react';
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const ModeContext = createContext(null);
 
 export default function ModeContextProvider({ children }) {
