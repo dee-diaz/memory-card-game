b9b51d64ba9952654f78f95dd1d8151fd43f7ca4
dee-diaz <diazthediablo@yahoo.com>
2026-03-25 22:36:46 -0300

    refactor(Scoreboard): migrate to TypeScript

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,4 +1,9 @@
-export default function Scoreboard({ score, bestScore }) {
+interface ScoreboardProps {
+  score: number;
+  bestScore: number;
+}
+
+export default function Scoreboard({ score, bestScore }: ScoreboardProps) {
   return (
     <div className="grid gap-3 justify-self-end text-white text-xl uppercase lg:text-2xl lg:justify-self-center lg:text-center">
       <p>Score: {score}</p>
